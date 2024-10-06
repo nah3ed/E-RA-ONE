03e0a06d7dc7ccedb6c5aba2c4e6bd7ea6250ea0
nah3ed <153137189+nah3ed@users.noreply.github.com>
2024-10-06 19:09:24 +0600

    Update E-RA ONE ERC20.sol

--- E-RA ONE ERC20.sol
+++ E-RA ONE ERC20.sol
@@ -7,11 +7,11 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract MyToken is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+contract E-RAONE is ERC20, ERC20Burnable, Ownable, ERC20Permit {
     constructor(address initialOwner)
-        ERC20("MyToken", "MTK")
+        ERC20("E-RA ONE", "E-RA")
         Ownable(initialOwner)
-        ERC20Permit("MyToken")
+        ERC20Permit("E-RA ONE")
     {}
 
     function mint(address to, uint256 amount) public onlyOwner {
